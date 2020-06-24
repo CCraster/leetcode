59ebf1d97806a8e90146914f4af9420ad4d874f2
Craster <xiongxiaoxiao1@xiaomi.com>
2020-06-24 16:56:57 +0800

    feat: add sample-1480

---
+++ src/1480.js
@@ -0,0 +1,28 @@
+/**
+ * https://leetcode-cn.com/problems/running-sum-of-1d-array/
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+// 84ms 22.50% 34.9MB 100.00%
+var runningSum = function (nums) {
+  let sum = 0;
+  return nums.map((value) => {
+    sum += value;
+    return sum;
+  });
+};
+
+// 72ms 77.31 35.1MB 100%
+var runningSum2 = function (nums) {
+  let sum = 0,
+    arr = [],
+    length = nums.length;
+  for (let i = 0; i < length; i++) {
+    sum += nums[i];
+    arr[i] = sum;
+  }
+  return arr;
+};
+
+// console.log(runningSum2([1, 2, 3, 4]));
---
+++ src/739.js
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode-cn.com/problems/daily-temperatures/
+ * @param {number[]} T
+ * @return {number[]}
+ */
+
+dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]);
+//[1, 1, 4, 2, 1, 1, 0, 0]
