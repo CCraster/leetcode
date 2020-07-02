7e7cbd9695f68cc86dba44d2372b57b431d8fbaf
Craster <xiongxiaoxiao1@xiaomi.com>
2020-07-02 18:33:37 +0800

    feat: add-mid-739

--- src/739.js
+++ src/739.js
@@ -3,6 +3,23 @@
  * @param {number[]} T
  * @return {number[]}
  */
+// n2
+var dailyTemperatures = function (T) {
+  let result = [];
+  for (let i = T.length - 1; i > -1; i--) {
+    result.unshift(getPos(i, T));
+  }
+  return result;
 
-dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]);
+  function getPos(index, arr) {
+    for (let i = index + 1; i < arr.length; i++) {
+      if (arr[i] > arr[index]) {
+        return i - index;
+      }
+    }
+    return 0;
+  }
+};
+
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]));
 //[1, 1, 4, 2, 1, 1, 0, 0]
